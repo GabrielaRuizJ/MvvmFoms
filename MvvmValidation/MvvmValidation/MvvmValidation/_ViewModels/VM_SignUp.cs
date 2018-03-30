8d91e2f74c719d0036694a822d315a2c3d5a8ead
Luis David De la cruz <delacruz_luis@outlook.es>
2018-03-29 23:21:30 -0600

    Mvvm ViewModel SignUp View SignUp

---
+++ MvvmValidation/MvvmValidation/MvvmValidation/_ViewModels/VM_SignUp.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MvvmValidation
+{
+    public class VM_SignUp
+    {
+        private string lblNombreUsuario ="Introduce tu nombre de usuario";
+        private string lblContraseña = "Introduce tu contraseña";
+        private string mensaje;
+        private string enUsuario;
+        private string enContraseña;
+        private bool ispassword = true;
+        private bool isEnable;
+
+        public bool IsEnable
+        {
+            get { return isEnable; }
+            set { isEnable = value; }
+        }
+
+        public bool Password
+        {
+            get { return ispassword; }
+            set { ispassword = value; }
+        }
+
+        public string LblContraseña
+        {
+            get { return lblContraseña; }
+            set { lblContraseña = value; }
+        }
+
+        public string LblUsuario
+        {
+            get { return lblNombreUsuario; }
+            set { lblNombreUsuario = value; }
+        }
+
+        public string EnUsuario
+        {
+            get { return enUsuario; }
+            set { enUsuario = value; }
+        }
+
+        public string EnContraseña
+        {
+            get { return enContraseña; }
+            set { enContraseña = value; }
+        }
+
+
+        public string Mensaje
+        {
+            get { return mensaje; }
+            set { mensaje = value; }
+        }
+
+
+    }
+}
