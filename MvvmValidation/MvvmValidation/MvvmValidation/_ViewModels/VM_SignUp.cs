43bce625b8c57d21e1e759adf86a1d2bc28ef339
Luis David De la cruz <delacruz_luis@outlook.es>
2018-03-30 00:29:39 -0600

    Validación completa/ Complete validation

--- MvvmValidation/MvvmValidation/MvvmValidation/_ViewModels/VM_SignUp.cs
+++ MvvmValidation/MvvmValidation/MvvmValidation/_ViewModels/VM_SignUp.cs
@@ -1,47 +1,106 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
+using Xamarin.Forms;
 
 namespace MvvmValidation
 {
-    public class VM_SignUp
+    public class VM_SignUp: INotifyPropertyChanged
     {
         private string lblNombreUsuario ="Introduce tu nombre de usuario";
         private string lblContraseña = "Introduce tu contraseña";
-        private string mensaje;
+        private string mensaje ="No se permiten cacteres especiales";
         private string enUsuario;
         private string enContraseña;
         private bool ispassword = true;
         private bool isEnable;
+        private bool visible;
+        private string mesajebutton="Mostrar contraseña";
+
+        public  VM_SignUp()
+        {
+            Clicked = new Command(ShowPass);
+        }
+
+        private void ShowPass(object obj)
+        {
+            Password = (Password == true) ? Password = false:Password=true;
+        }
+
+        public Command Clicked{ get; }
+        public string MensajeButton
+        {
+            get { return mesajebutton; }
+            set { mesajebutton = value; }
+        }
+
+        public bool IsVisible
+        {
+            get { return visible; }
+            set { visible = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Mensaje));
+            }
+        }
+
 
         public bool IsEnable
         {
             get { return isEnable; }
-            set { isEnable = value; }
+            set { isEnable = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Mensaje));
+            }
         }
 
         public bool Password
         {
             get { return ispassword; }
-            set { ispassword = value; }
+            set { ispassword = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Mensaje));
+            }
         }
 
         public string LblContraseña
         {
             get { return lblContraseña; }
-            set { lblContraseña = value; }
+            set { lblContraseña = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Mensaje));
+            }
         }
 
         public string LblUsuario
         {
             get { return lblNombreUsuario; }
-            set { lblNombreUsuario = value; }
+            set {
+                lblNombreUsuario = value;
+            }
         }
 
         public string EnUsuario
         {
             get { return enUsuario; }
-            set { enUsuario = value; }
+            set {
+                enUsuario = value;
+                if (enUsuario.Contains("=") || enUsuario.Contains("'") || enUsuario.Contains("?")
+                    || enUsuario.Contains("/") || enUsuario.Contains("$") || enUsuario.Contains("#")
+                    || enUsuario.Contains("|") || enUsuario.Contains(" "))
+                {
+                    IsVisible = true;
+                    IsEnable = false;
+                }
+                else
+                {
+                    IsVisible = false;
+                    IsEnable = true;
+                }
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Mensaje));
+            }
         }
 
         public string EnContraseña
@@ -57,6 +116,10 @@ namespace MvvmValidation
             set { mensaje = value; }
         }
 
-
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
